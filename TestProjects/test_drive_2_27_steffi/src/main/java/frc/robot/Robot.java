5a108117bdaa5059110e7b5c9e81a35a0a8e72d5
steffimathew33 <steffimathew33@gmail.com>
2022-02-27 14:46:45 -0800

    added a new test project called test_drive_2_27_steffi in order to test drive. before testing driver control

---
+++ TestProjects/test_drive_2_27_steffi/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,136 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  private static Timer timer = new Timer();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    //RobotContainer.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+    
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic()
+  {
+    /* what was happening on 2-27:
+       left joystick: left/right
+       right joystick: absolutely nothing
+       need to get rid of arcade drive
+    */
+    // we dont want this RobotContainer.drive.arcadeDrive(-1 * RobotContainer.joy.getAxis(0) * Constants.driveLimitCoefficient, RobotContainer.joy.getRawAxis(5) * Constants.driveLimitCoefficient);
+    //RobotContainer.intakeMotor.set(RobotContainer.joy.getRawAxis(5));
+    //RobotContainer.shooterMotor.set(RobotContainer.joy.getRawAxis(1));
+
+    /*
+    Help references:
+         https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj/drive/DifferentialDrive.html
+         https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robotbuilder/writing-code/robotbuilder-drive-tank.html
+
+    */
+    
+    /*
+      FOR THE FOLLOWING LINE:
+      WE NEED TO MAKE SURE THAT:
+        1. THE LEFT SIDE OF TANK (1st parameter) IS THE X-AXIS ON LEFT JOYSTICK
+        2. THE RIGHT SIDE OF TANK (2nd parameter) IS THE Y-AXIS ON RIGHT JOYSTICK
+
+      need to check via DriverStation
+      
+    */
+    RobotContainer.drive.tankDrive(-1 * RobotContainer.controller_1.getAxis(0) * Constants.driveLimitCoefficient, RobotContainer.controller_1.getxis(5) * Constants.driveLimitCoefficient);
+
+    // DO I NEED TWO OF THESE TO DO TWO DIFFERENT JOYSTICKS
+    // RobotContainer.drive.tankDrive(RobotContainer.controller_1.getAxis(0) * Constants.driveLimitCoefficient, RobotContainer.controller_1.getxis(5) * Constants.driveLimitCoefficient);
+
+    RobotContainer.intakeMotor(-1 * RobotContainer.controller_1.getAxis(0) * Constants.driveLimitCoefficient, RobotContainer.controller_1.getxis(5) * Constants.driveLimitCoefficient);
+    
+    
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
---
+++ TestProjects/test_drive_2_27_steffi/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,116 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+ 
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  
+
+  public static WPI_TalonSRX[] leftDriveMotors;
+  public static WPI_TalonSRX[] rightDriveMotors;
+  
+
+  public static MotorControllerGroup leftDriveController;
+  public static MotorControllerGroup rightDriveController;
+
+  public static DifferentialDrive drive;
+
+  public static WPI_TalonSRX intakeMotor;
+
+  public static WPI_TalonFX climbMotor;
+  public static WPI_TalonFX shooterMotor;
+
+  public static Joystick controller_1;
+
+  public static Joystick controller_2;
+
+  public static AHRS navX;
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+    leftDriveMotors = new WPI_TalonSRX[Constants.leftDriveMotorIDs.length];
+    rightDriveMotors = new WPI_TalonSRX[Constants.rightDriveMotorIDs.length];
+    
+    
+    for(int i = 0; i < Constants.leftDriveMotorIDs.length; i++) // left side
+    {
+      leftDriveMotors[i] = new WPI_TalonSRX(Constants.leftDriveMotorIDs[i]);
+      leftDriveMotors[i].setInverted(Constants.leftDriveMotorInverts[i]);
+    }
+
+    for(int i = 0; i < Constants.rightDriveMotorIDs.length; i++) // right side
+    {
+      rightDriveMotors[i] = new WPI_TalonSRX(Constants.rightDriveMotorIDs[i]);
+      rightDriveMotors[i].setInverted(Constants.rightDriveMotorInverts[i]);
+    }
+    
+
+    leftDriveController = new MotorControllerGroup(leftDriveMotors);
+    rightDriveController = new MotorControllerGroup(rightDriveMotors);
+
+    drive = new DifferentialDrive(leftDriveController, rightDriveController);
+    drive.setDeadband(Constants.deadbandThreshold);
+
+    intakeMotor = new WPI_TalonSRX(Constants.intakeMotorID);
+    
+    controller_1 = new Joystick(Constants.joystickID);
+
+    controller_2 = new Joystick(Constants.joystickID); // NEEDS NEW ID
+
+    climbMotor = new WPI_TalonFX(Constants.climbFalconMotorID);
+    shooterMotor = new WPI_TalonFX(Constants.shooterFalconMotorID);
+
+    navX = new AHRS(SerialPort.Port.kMXP);
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    //final ExampleCommand sprint = new OneSecondSprint(leftDriveMotors, rightDriveMotors);
+    //return sprint;
+    return m_autoCommand;
+  }
+  
+
+}
